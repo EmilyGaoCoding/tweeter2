b6c016a6b02920c91ce16b480073e1ed104f7c73
Emily Gao <emily.s.gao@gmail.com>
2018-01-30 15:38:11 -0700

    added new tweet character count function

---
+++ public/scripts/composer-char-count.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  
+  function updateCount() {
+    const charLeft = 140 - $(this).val().length;
+    $(this).siblings('.counter').text(charLeft);
+    
+    if (charLeft < 0) {
+      $(this).siblings('.counter').addClass('neg');
+    }
+  }
+  $('form').on('keyup', 'textarea', updateCount);
+
+});
