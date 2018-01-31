7e37e9c30276dc3484636fb3fa7b465d108f27ba
Emily Gao <emily.s.gao@gmail.com>
2018-01-30 19:23:22 -0700

    added createTweetElement function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,36 @@
 
 $(document).ready(function() {
   
-
+  $('#tweets-container').append($tweet); 
+  function createTweetElement(tweetData) {
+    const article = $('<article>');
+    article.addClass('tweet').appendTo('.tweets');
+    
+    const header = $('<header>');
+    const divImg = $('<div>');
+    const img = $('<img>', {
+      'src': tweetData.user.avatars.small 
+    });
+    const divUsername = $('<div>', {
+      'class': 'username',
+      text: tweetData.user.name
+    });
+    const divHandle = $('<div>', {
+      'class': 'handle',
+      text: tweetData.user.handle
+    });
+    const p = $('<p>', {
+      text: tweetData.content.text
+    });
+    const footer = $("<footer>", {
+      text: Math.round(((Date.now() - tweetData.created_at) / 86400000))  + ' days ago'
+    });
+    
+    div1.append(img);
+    header.append(divImg, divUsername, divHandle);
+    article.append(header, p, footer);
+  }
   
 });
+
+
